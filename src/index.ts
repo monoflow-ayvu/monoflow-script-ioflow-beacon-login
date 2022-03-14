027222b53c5cc304c56bb1e5587381d45d843eea
Fernando Mumbach <fermuch@ayvu.net>
2022-03-14 09:01:06 -0300

    migrating name to new namespace

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import * as MonoUtils from "@fermuch/monoutils";
 
 // based on settingsSchema @ package.json
-type Config = Record<string, unknown> & {
-  nome: string;
-}
-
+type Config = {};
 const conf = new MonoUtils.config.Config<Config>();
 
 messages.on('onInit', function() {
-  platform.log('script started! (I am the script)');
-  platform.log('settings:');
-  platform.log(conf.store);
-
-  const name = conf.get('nome', 'default name');
-  platform.log(`Hello, ${name}!`);
+  platform.log('GPS script started');
 });
